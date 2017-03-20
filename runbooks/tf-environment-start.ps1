fa8566569cfbddf09a5f90f478c81fa451f3b889
rvodden <richard@vodden.com>
2017-03-20 11:48:14 +0000

    Created tf-environment-start.ps1 in AzureAutomation.

---
+++ runbooks/tf-environment-start.ps1
@@ -0,0 +1,11 @@
+workflow tf-environment-start
+{
+    $Conn = Get-AutomationConnection -Name AzureRunAsConnection
+    Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+    Start-AzureRmVM -Name 'rdv-tfs-ad-1' -ResourceGroupName 'tfs'
+    Parallel 
+    {
+        Start-AzureRmVM -Name 'rdv-tfs-db-1' -ResourceGroupName 'tfs'
+        Start-AzureRmVM -Name 'rdv-tfs-tfs-1' -ResourceGroupName 'tfs'
+    }
+}
