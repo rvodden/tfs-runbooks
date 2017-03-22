33e47359bfead7509145e05fde3ed0f7ff489880
rvodden <richard@vodden.com>
2017-03-22 12:01:47 +0000

    Updated tf-environment-start.ps1 in Azure Automation.

--- runbooks/tf-environment-start.ps1
+++ runbooks/tf-environment-start.ps1
@@ -8,5 +8,6 @@ workflow tf-environment-start
         Start-AzureRmVM -Name 'rdv-tfs-db-1' -ResourceGroupName 'tfs'
         Start-AzureRmVM -Name 'rdv-tfs-tfs-1' -ResourceGroupName 'tfs'
         Start-AzureRmVM -Name 'rdv-tfs-sp-1' -ResourceGroupName 'tfs'
+        Start-AzureRmVM -Name 'rdv-tfs-rdp-1' -ResourceGroupName 'tfs'
     }
 }
